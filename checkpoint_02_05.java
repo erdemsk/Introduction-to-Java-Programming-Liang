4864a1891f73838d013640801c6830854044c310
Erdem Şık <sk.erdems@gmail.com>
2021-07-21 22:33:58 +0300

    Delete checkpoint_02_05.java

--- checkpoint_02_05.java
+++
@@ -1,9 +0,0 @@
-//Introduction to Java Programming and Data Structures, Liang, 11th Edition, 2.5 Variables, 64
-
-public class checkpoint_02_05 {
-    public static void main(String[] args) {
-        int k=1;
-        int i=k+2;
-        System.out.println(i);
-    }
-}
