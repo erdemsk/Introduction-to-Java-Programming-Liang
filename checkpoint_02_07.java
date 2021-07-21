55a4858119e4ec424a2cbc08eea4617aee4b8a51
Erdem Şık <sk.erdems@gmail.com>
2021-07-21 22:34:08 +0300

    Delete checkpoint_02_07.java

--- checkpoint_02_07.java
+++
@@ -1,9 +0,0 @@
-//Introduction to Java Programming and Data Structures, Liang, 11th Edition, 2.7 Named Constants, 66
-
-public class checkpoint_02_07 {
-    public static void main(String[] args) {
-        final int SIZE = 20;
-
-        System.out.println("SIZE constant is: "+SIZE);
-    }
-}
