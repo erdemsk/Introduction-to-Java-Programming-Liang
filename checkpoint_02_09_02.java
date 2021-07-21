f143a894de16a6fe947c107c56ce5038aca76fc7
Erdem Şık <sk.erdems@gmail.com>
2021-07-21 22:34:18 +0300

    Delete checkpoint_02_09_02.java

--- checkpoint_02_09_02.java
+++
@@ -1,8 +0,0 @@
-//Introduction to Java Programming and Data Structures, Liang, 11th Edition, 2.9.4 Exponent Operations, 70
-public class checkpoint_02_09_02 {
-    public static void main(String[] args) {
-        System.out.println("78%-4 is: "+ 78%-4);
-        System.out.println("-34%5 is :"+ -34%5);
-        System.out.println("-34%-5 is: "+ -34%-5);
-    }
-}
