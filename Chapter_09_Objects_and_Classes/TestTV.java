80512b2e5fc7ca6d1eddc6210eaaf92481f70661
Erdem Şık <sk.erdems@gmail.com>
2021-09-16 08:02:03 +0300

    Add files via upload

---
+++ Chapter_09_Objects_and_Classes/TestTV.java
@@ -0,0 +1,20 @@
+/* Introduction to Java Programming and Data Structures, Liang, 11th Edition,
+9.3 Example: Defining Classes and Creating Objects, 352*/
+public class TestTV {
+    public static void main(String[] args) {
+        TV tv1 = new TV();
+        tv1.turnOn();
+        tv1.setChannel(30);
+        tv1.setVolume(3);
+
+        TV tv2 = new TV();
+        tv2.turnOn();
+        tv2.channelUp();
+        tv2.channelUp();
+        tv2.volumeUp();
+
+        System.out.println("tv1's channel is " + tv1.channel + " and volume level is " + tv1.volumeLevel);
+        System.out.println("tv2's channel is " + tv2.channel + " and volume level is " + tv2.volumeLevel);
+    }
+}
+
