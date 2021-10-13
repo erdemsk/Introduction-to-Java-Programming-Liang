c6a31cd48a8734970c8548a02480114debdd8b35
Erdem Şık <sk.erdems@gmail.com>
2021-10-13 19:52:45 +0300

    Add files via upload

--- Chapter_07_Single-Dimensional_Arrays/BinarySearch.java
+++ Chapter_07_Single-Dimensional_Arrays/BinarySearch.java
@@ -1,25 +1,34 @@
 /* Introduction to Java Programming and Data Structures, Liang, 11th Edition,
-7.13 Command-Line Arguments, 298*/
+7.10 Searching Arrays, 292*/
 public class BinarySearch {
     public static void main(String[] args) {
-        //Check the number of Strings passed
-        if (args.length != 3){
-            System.out.println("Usage: java Calculator operand1 operator operand2");
-            System.exit(1);
+        int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+        int i = BinarySearch.binarySearch(list, 2); //returns 0
+        int j = BinarySearch.binarySearch(list, 11); //returns 4
+        int k = BinarySearch.binarySearch(list, 12); //returns -6
+        int l = BinarySearch.binarySearch(list, 1); //returns -1
+        int m = BinarySearch.binarySearch(list, 3); //returns -2
+
+        //display the results
+        System.out.println("Binary search results are:\ni: " + i + "\nj: " + j + "\nk: " + k +
+                "\nl: " + l + "\nm: " + m);
+    }
+
+    public static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int high = list.length - 1;
+
+        while (high >= low) {
+            int mid = (low + high) / 2;
+
+            if (key < list[mid]) {
+                high = mid - 1;
+            } else if (key == list[mid]) {
+                return mid;
+            } else {
+                low = mid + 1;
+            }
         }
-
-        //The result of the operation
-        int result = 0;
-
-        //determine the operator
-        switch (args[1].charAt(0)){
-            case '+': result = Integer.parseInt(args[0]) + Integer.parseInt(args[2]); break;
-            case '-': result = Integer.parseInt(args[0]) - Integer.parseInt(args[2]); break;
-            case '.': result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]); break;
-            case '/': result = Integer.parseInt(args[0]) / Integer.parseInt(args[2]); break;
-        }
-
-        //display the result
-        System.out.println(args[0] + ' ' + args[1] + ' ' + args[2] + " = " + result);
+        return -low - 1;
     }
 }
